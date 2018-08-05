acb4c6e2186dccd458cb55ca640a5a268db441e7
Jota Filho <jotal18@hotmail.com>
2018-08-05 13:00:01 -0300

    Site - Categorias

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $app->get('/admin', function() {
 });
 
 $app->get('/admin/login', function() {
-    
+
 	$page = new PageAdmin(array(
 		"header"=>false,
 		"footer"=>false
@@ -266,7 +266,7 @@ $app->get("/admin/categories/:idcategory", function($idcategory){
 
 $app->post("/admin/categories/:idcategory", function($idcategory){
 
-	User::verifyLogin();
+	User::verifyLogin();	
 
 	$category = new Category();
 
@@ -278,6 +278,21 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 
 	header("Location: /admin/categories");
 	exit;
+
+});
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+		
+	$page->setTpl("category",[
+		'category'=>$category->getValues(),
+		'produtos'=>[]
+	]);
 
 });
 
